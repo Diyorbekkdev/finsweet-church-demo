0cf80fba7982ac5e3a6263a46d9f27a6e73de885
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-04-11 00:26:40 +0500

    finished

--- Js/main.js
+++ Js/main.js
@@ -31,40 +31,13 @@ function showSlides() {
 
 
 
-// var i = 0;
-// var titles = [  "Diyorbek | Home",  "Jo'rabek | About",  "Nodirbek | Blogs",  "Abdulaziz | Sermons", "Behruzbek | Contact"];
-
-// function changeTitle() {
-//   document.title = titles[i];
-//   i = (i + 1) % titles.length;
-//   setTimeout(changeTitle, 750); // Change title every 2 seconds
-// }
-
-// changeTitle();
-
-var titleIcons = {
-  "Title 1": "",
-  "Title 2": "icon2.png",
-  "Title 3": "icon3.png",
-  "Title 4": "icon4.png",
-  "Title 5": "icon5.png"
-};
-
-function setTitleIcon(title) {
-  var icon = titleIcons[title];
-  if (icon) {
-    var link = document.getElementById('page-icon');
-    link.href = icon;
-  }
-}
+var i = 0;
+var titles = [  "Diyorbek | Home",  "Jo'rabek | About",  "Nodirbek | Blogs",  "Abdulaziz | Sermons", "Behruzbek | Contact"];
 
 function changeTitle() {
-  var titles = Object.keys(titleIcons);
-  var i = Math.floor(Math.random() * titles.length);
-  var title = titles[i];
-  document.title = title;
-  setTitleIcon(title);
-  setTimeout(changeTitle, 750); // Change title every 2 seconds
+  document.title = titles[i];
+  i = (i + 1) % titles.length;
+  setTimeout(changeTitle, 750); 
 }
 
 changeTitle();
