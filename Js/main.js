982f4b834d881d3a1ebde08035f6c10c662f8951
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-04-10 20:40:02 +0500

    finished

--- Js/main.js
+++ Js/main.js
@@ -29,3 +29,42 @@ function showSlides() {
   setTimeout(showSlides, 5000);
 }
 
+
+
+// var i = 0;
+// var titles = [  "Diyorbek | Home",  "Jo'rabek | About",  "Nodirbek | Blogs",  "Abdulaziz | Sermons", "Behruzbek | Contact"];
+
+// function changeTitle() {
+//   document.title = titles[i];
+//   i = (i + 1) % titles.length;
+//   setTimeout(changeTitle, 750); // Change title every 2 seconds
+// }
+
+// changeTitle();
+
+var titleIcons = {
+  "Title 1": "",
+  "Title 2": "icon2.png",
+  "Title 3": "icon3.png",
+  "Title 4": "icon4.png",
+  "Title 5": "icon5.png"
+};
+
+function setTitleIcon(title) {
+  var icon = titleIcons[title];
+  if (icon) {
+    var link = document.getElementById('page-icon');
+    link.href = icon;
+  }
+}
+
+function changeTitle() {
+  var titles = Object.keys(titleIcons);
+  var i = Math.floor(Math.random() * titles.length);
+  var title = titles[i];
+  document.title = title;
+  setTitleIcon(title);
+  setTimeout(changeTitle, 750); // Change title every 2 seconds
+}
+
+changeTitle();
