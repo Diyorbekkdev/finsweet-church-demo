4ca7886e47be716c627805f2fea93b141386a492
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-04-10 12:12:10 +0500

    first commit

---
+++ Js/main.js
@@ -0,0 +1,31 @@
+const navbar = document.getElementById('navbar');
+let prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.transform = "translateY(0)";
+  } else {
+    navbar.style.transform = "translateY(-80px)";
+  }
+  prevScrollPos = currentScrollPos;
+}
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 5000);
+}
+
